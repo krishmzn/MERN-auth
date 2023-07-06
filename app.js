00405b9dcfb138bc9ce44e779ccddc51ca40babe
Krish <krishmzn69@gmail.com>
2023-07-06 16:27:45 +0545

    port update

--- app.js
+++ app.js
@@ -6,21 +6,17 @@ const userRouter = require('./src/routes/userRoutes');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 
+// setting auth api api
 app.use("/api", userRouter);
 
 
-
+// homepage
 app.get("/", (req, res) => {
     res.send("Hello")
 });
 
 
-
-
-
-
-app.listen(3000, () => console.log('Server started on port 3000'));
-console.log('app.js');
+// starting server
+app.listen(5000, () => console.log('Server listening on port 5000'));
--- src/models/user.js
+++ src/models/user.js
@@ -16,5 +16,3 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
-
-console.log('user.js');
